c2f6d00295246dae2e8d52432239453914470193
kb-dev-sketch <kb61406@gmail.com>
2026-06-23 23:41:02 +0530

    brandSchema

---
+++ backend/src/model/brandProfile.js
@@ -0,0 +1,110 @@
+import mongoose from "mongoose";
+const brandSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique: true,
+    },
+    companyName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    logo: {
+      type: String,
+      default: "",
+    },
+    industry: {
+      type: String,
+      required: true,
+      trim: true,
+      enum: [
+        "Technology",
+        "Fashion",
+        "Food",
+        "Travel",
+        "Finance",
+        "Educating",
+        "Gaming",
+        "Healthcare",
+        "Beauty",
+        "Lifestyle",
+        "other",
+      ],
+    },
+    website: {
+      type: String,
+      trim: true,
+      default: "",
+    },
+    description: {
+      type: String,
+      maxLength: 1000,
+      default: "",
+    },
+    companySize: {
+      type: String,
+      enum: ["1-10", "11-50", "51-100", "201-500", "500+"],
+      default: "1-10",
+    },
+    location: {
+      type: String,
+      trim: true,
+      default: "",
+    },
+    socials: {
+      instagram: {
+        type: String,
+        trim: true,
+        default: "",
+      },
+      linkedin: {
+        type: String,
+        trim: true,
+        default: "",
+      },
+      x: {
+        type: String,
+        trim: true,
+        default: "",
+      },
+    },
+    totalCampaigns: {
+      type: Number,
+      default: 0,
+    },
+    averageRating: {
+      type: Number,
+      default: 0,
+      min: 0,
+      max: 0,
+    },
+    totalReviews: {
+      type: Boolean,
+      default: 0,
+    },
+    profileCompleted: {
+      type: Boolean,
+      default: false,
+    },
+    isVerified: {
+      type: Booolean,
+      default: false,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timeStamps },
+);
+
+// Search indexes
+
+brandSchema.index({ companyName: 1 });
+brandSchema.index({ industry: 1 });
+brandSchema.index({ location: 1 });
+
+export const Brand = mongoose.Model("Brand", brandSchema);
--- backend/src/model/user.model.js
+++ backend/src/model/user.model.js
@@ -1,11 +1,32 @@
 import mongoose from"mongoose"
 
-const user=new mongoose.model({
-    {
-    username:String,
-    require:true,
+const userSchema=new mongoose.Schema({
+    
+    username:{
+        type:String,
+         required:true,
+         lowercase:true,
+         trim:true,
+         index:true,
     },
-    {
-        
-    }
-})
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+    },
+    password:{
+        type:String,
+        unique:true,
+        required:true
+    },
+    role:{
+        type:String,
+        enum:["brand","creator"],
+        reuired:true,
+    },
+},
+{timestamps:true}
+)
+export const User=mongoose.model("User",userSchema)
